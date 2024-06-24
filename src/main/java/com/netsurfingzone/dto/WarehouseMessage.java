24484e7d7495b88a0abf7506aefe527675bd86ac
liaosifish <tessa20030807@hotmail.com>
2024-06-24 21:02:26 +0800

    仓库消息结构更新

--- src/main/java/com/netsurfingzone/dto/WarehouseMessage.java
+++ src/main/java/com/netsurfingzone/dto/WarehouseMessage.java
@@ -1,4 +1,79 @@
 package com.netsurfingzone.dto;
 
+import java.util.UUID;
+
 public class WarehouseMessage {
+    // Common Fields for All Messages
+    private String messageId;
+    private String status; //状态：出库/入库
+    private String warehouse;//仓库，可作为进入也可作为出口
+    private String catId; // 自动车辆ID
+    private String liftId;//升降机ID
+    private String goodId;
+    private int quantity;
+    private String destination; //仓库之外的目的地
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(String warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    public String getCatId() {
+        return catId;
+    }
+
+    public void setCatId(String catId) {
+        this.catId = catId;
+    }
+
+    public String getLiftId() {
+        return liftId;
+    }
+
+    public void setLiftId(String liftId) {
+        this.liftId = liftId;
+    }
+
+    public String getGoodId() {
+        return goodId;
+    }
+
+    public void setGoodId(String goodId) {
+        this.goodId = goodId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
 }
